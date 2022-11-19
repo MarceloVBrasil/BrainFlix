1751bde9ccdd6ac84ce8cca52e7a04b6799debed
MarceloVBrasil <marcelo.vital.brasil@gmail.com>
2022-11-19 13:53:09 -0500

    stay at the same video page on reload

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -19,7 +19,7 @@ export default function HomePage() {
 
   useEffect(() => {
     apiKeyPromise = register();
-    getDefaultVideoId();
+    if (!id) getDefaultVideoId();
   }, []);
 
   useEffect(() => {
